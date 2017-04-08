598f88273ebcb7dd759fcc9650ef1c74f46f81cc
Alyssa <seibelalyssa@gmail.com>
2017-04-08 14:21:19 -0500

    design stuff

--- app/src/main/java/com/example/alyssa/capstone/MainActivity.java
+++ app/src/main/java/com/example/alyssa/capstone/MainActivity.java
@@ -102,28 +102,6 @@ public class MainActivity extends AppCompatActivity {
         checkForPermission();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void checkForPermission() {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, 1);
